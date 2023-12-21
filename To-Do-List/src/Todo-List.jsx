ff35caba8d0dcae71e84624b8d859508d57257f2
SHREYANK <66244570+bshreyank@users.noreply.github.com>
2023-12-22 00:41:31 +0530

    Todo

--- To-Do-List/src/Todo-List.jsx
+++ To-Do-List/src/Todo-List.jsx
@@ -38,41 +38,37 @@ const TodoList = () => {
   let data = ''
   const handleForm = (e) => {
     e.preventDefault()
-    dispatch({ type: 'ADD', payload: data })
-    console.log(e.target.value)
+    dispatch({
+      type: 'ADD',
+      payload: data,
+    })
   }
 
   const [todo, dispatch] = useReducer(reducer, initialValue)
 
   return (
-    <div style={{ textAlign: 'center' }}>
-      <div style={{ marginTop: 30, marginBottom: 30 }}>
-        <h1>To Do List</h1>
-        <form onSubmit={handleForm}>
-          <input type="text" onChange={(e) => (data = e.target.value)} />
-          <button type="submit">Add</button>
-        </form>
+    <>
+      <div style={{ textAlign: 'center' }}>
+        <div style={{ marginTop: 30, marginBottom: 30 }}>
+          <h1>To do List</h1>
+          <form onSubmit={handleForm}>
+            <input type="text" onChange={(e) => (data = e.target.value)} />
+            <button type="submit"> Add </button>
+          </form>
+        </div>
+        {todo.map((m) => (
+          <li key={m.id}>
+            <input type="checkbox" />
+
+            {m.task}
+
+            <button onClick={() => dispatch({ type: 'DEL', payload: m.id })}>
+              Delete
+            </button>
+          </li>
+        ))}
       </div>
-      {todo.map((m) => (
-        <li key={m.id}>
-          {m.task}
-        
-          <input type="checkbox" />
-
-          <span
-              style={{
-                textDecoration: todo.completed ? 'line-through' : 'none',
-              }}
-            >
-              {todo.text}
-            </span>
-
-          <button onClick={() => dispatch({ type: 'DEL', payload: m.id })}>
-            Delete
-          </button>
-        </li>
-      ))}
-    </div>
+    </>
   )
 }
 
